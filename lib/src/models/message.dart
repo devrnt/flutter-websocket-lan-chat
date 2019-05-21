dc68783bf82683113499c420594c16d545aab95e
devrnt <devrient.jonas@gmail.com>
2019-05-21 19:22:07 +0200

    Update message model Add uuid

--- lib/src/models/message.dart
+++ lib/src/models/message.dart
@@ -1,13 +1,17 @@
 import 'package:websocket_lan_chat/src/models/user.dart';
 
 class Message {
+  String id;
   User author;
   String body;
 
-  Message({this.author, this.body});
+  Message({this.id, this.author, this.body});
 
-  factory Message.fromJson(Map<String, dynamic> json) =>
-      new Message(author: User.fromJson(json['author']), body: json['body']);
+  factory Message.fromJson(Map<String, dynamic> json) => new Message(
+      id: json['id'],
+      author: User.fromJson(json['author']),
+      body: json['body']);
 
-  Map<String, dynamic> toJson() => {'author': author.toJson(), 'body': body};
+  Map<String, dynamic> toJson() =>
+      {'id': id, 'author': author.toJson(), 'body': body};
 }
