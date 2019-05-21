1eada4a37d83cb12c0442d2a7c0074cf61718de8
devrnt <devrient.jonas@gmail.com>
2019-05-21 17:28:28 +0200

    Add home screen with config file

---
+++ lib/config/config.dart
@@ -0,0 +1,4 @@
+class Config {
+  static String ip = '192.168.0.229';
+  static int port = 8080;
+}
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,35 +1,90 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/status.dart' as status;
+import 'package:websocket_lan_chat/config/config.dart';
+import 'package:websocket_lan_chat/src/widgets/message_input.dart';
+import 'package:websocket_lan_chat/src/widgets/message_list.dart';
+import 'package:websocket_lan_chat/src/models/user.dart';
+import 'package:websocket_lan_chat/src/models/message.dart';
 
-class HomeScreen extends StatelessWidget {
-  final channel = IOWebSocketChannel.connect('ws://192.168.0.229:8080');
+// TODO: move to provdider
+User _user = new User(
+    name: 'Jonas',
+    imageUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    color: Colors.green);
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final IOWebSocketChannel channel =
+      IOWebSocketChannel.connect('ws://${Config.ip}:${Config.port}');
+
+  final TextEditingController _textEditingController =
+      new TextEditingController();
+
+  List<Message> messages = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder(
-        stream: channel.stream,
-        builder: (context, snapshot) {
-          if(snapshot.hasError){
-            return Center(child: Text(snapshot.error.toString()));
-          }
-          return Container(
-              color: Colors.pink,
-              child: Center(
-                  child: Text(snapshot.hasData ? '${snapshot.data}' : '')));
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.call_missed_outgoing),
-        onPressed: _sendMessage,
+      body: Container(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 9,
+              child: StreamBuilder(
+                stream: channel.stream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(child: Text(snapshot.error.toString()));
+                  }
+
+                  if (snapshot.data == null) {
+                    return Center(
+                        child: Text('No messages yet, start typing...'));
+                  } else {
+                    Message message =
+                        Message.fromJson(jsonDecode(snapshot.data));
+                    messages.add(message);
+                  }
+                  return MessageList(messages: messages);
+                },
+              ),
+            ),
+            Expanded(
+              child: MessageInput(
+                textEditingController: _textEditingController,
+                onPressed: _sendMessage,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 
+  @override
+  void dispose() {
+    channel.sink.close(status.goingAway);
+    super.dispose();
+  }
+
   void _sendMessage() {
-    // TODO: is LAN ip working or is localhost needed
-    channel.sink.add('Hello!');
+    final messageBody = _textEditingController.text;
+
+    final Message message = new Message(author: _user, body: messageBody);
+    final jsonMessage = jsonEncode(message);
+
+    channel.sink.add(jsonMessage);
+
+    _textEditingController.clear();
   }
 }
